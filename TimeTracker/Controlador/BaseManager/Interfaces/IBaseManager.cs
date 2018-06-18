e785b2e16cca0e1cc85d8ef2a941e3001e5924b5
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-06-18 09:23:34 -0300

    Criado a interface Base Manager

---
+++ TimeTracker/Controlador/BaseManager/Interfaces/IBaseManager.cs
@@ -0,0 +1,20 @@
+﻿#region --Using--
+using System.Collections.Generic;
+
+#endregion
+
+namespace Controlador.BaseManager.Interfaces
+{
+    public interface IBaseManager<T> where T : class
+    {
+
+        #region --Métodos--
+        void Inserir(T entidade);
+        void Inativar(T entidade);
+        void Atualizar(T entidade);
+        T Carregar(int ID);
+        List<T> CarregarTodos();
+        #endregion
+
+    }
+}
