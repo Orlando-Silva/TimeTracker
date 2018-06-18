18c499446cc089a5bd58b07db28238037e6b980e
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-06-18 09:02:09 -0300

    DAL.Contexto Modificado

--- TimeTracker/Controlador/DAL/Contexto.cs
+++ TimeTracker/Controlador/DAL/Contexto.cs
@@ -1,15 +1,26 @@
 ﻿#region --Using--
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Modelos.Entidades;
+using System.Data.Entity;
 #endregion
 
 namespace Controlador.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+
+        #region --Construtor--
+        public Contexto() : base("ConnectionString")
+        {
+
+        }
+        #endregion
+
+        #region --Entidades--
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Atividade> Atividades { get; set; }
+        public DbSet<Time> Time { get; set; }
+        public DbSet<Membro> Membro { get; set; }
+        #endregion
 
     }
 }
