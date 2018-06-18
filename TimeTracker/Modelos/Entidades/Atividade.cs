05825075afd7a4c35aa220d48dcd337b25afe810
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-06-18 09:02:47 -0300

    Removendo Using desnecessários

--- TimeTracker/Modelos/Entidades/Atividade.cs
+++ TimeTracker/Modelos/Entidades/Atividade.cs
@@ -1,9 +1,5 @@
 ﻿#region --Using--
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 #endregion
 
 namespace Modelos.Entidades
