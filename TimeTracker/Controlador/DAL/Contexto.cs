0874dcf75895f4f49bd8080a497bada8dc66e716
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-06-18 08:57:42 -0300

    Criado DAL.

---
+++ TimeTracker/Controlador/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿#region --Using--
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace Controlador.DAL
+{
+    public class Contexto
+    {
+
+    }
+}
