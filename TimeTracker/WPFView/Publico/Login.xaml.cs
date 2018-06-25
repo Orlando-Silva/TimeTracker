c617d28615ed05dcd255b252e38cb21edd623519
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-06-25 11:15:55 -0300

    Adicionado tela de Login e novo usuário

--- TimeTracker/WPFView/Publico/Login.xaml.cs
+++ TimeTracker/WPFView/Publico/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#region --Using--
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPFView.Publico;
+#endregion
 
 namespace WPFView
 {
@@ -19,9 +22,26 @@ namespace WPFView
     /// </summary>
     public partial class Login : Window
     {
+        #region --Construtor--
         public Login()
         {
             InitializeComponent();
+            LabelCriarLogin.Content = "Não possuo login";
         }
+        #endregion
+
+        #region --Eventos--
+        private void LabelCriarLogin_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            new NovoUsuario().Show();
+            this.Close();
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
     }
 }
---
+++ TimeTracker/WPFView/Publico/NovoUsuario.xaml.cs
@@ -0,0 +1,67 @@
+﻿#region --Using--
+using BespokeFusion;
+using BusinessLogic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+#endregion
+
+namespace WPFView.Publico
+{
+    /// <summary>
+    /// Interaction logic for NovoUsuario.xaml
+    /// </summary>
+    public partial class NovoUsuario : Window
+    {
+        #region --Construtor--
+        public NovoUsuario()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region --Eventos Click--
+        private void ButtonCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            if(IsValid())
+            {
+                if(SenhasMatch())
+                {
+                    try
+                    {
+                        new UsuarioService().Inserir(TextBoxNome.Text, TextBoxLogin.Text, TextBoxSenha.Password);
+                    }
+                    catch(Exception erro)
+                    {
+                        MaterialMessageBox.Show(erro.Message, "Erro");
+                    }
+                }
+                else
+                {
+                    MaterialMessageBox.Show("Os campos 'Senha' e 'Confirmar senha' não batem!", "Erro");
+                }
+            }
+            else
+            {
+                MaterialMessageBox.Show("Para prosseguir, preencha todos os campos!", "Erro");
+            }
+        }
+        #endregion
+
+        #region --Métodos--
+        private bool IsValid() => !String.IsNullOrWhiteSpace(TextBoxLogin.Text) && !String.IsNullOrWhiteSpace(TextBoxNome.Text)
+                               && !String.IsNullOrWhiteSpace(TextBoxSenha.Password) && !String.IsNullOrWhiteSpace(TextBoxConfirmarSenha.Password);
+        private bool SenhasMatch() => TextBoxSenha.Password.ToString() == TextBoxConfirmarSenha.Password.ToString();
+        #endregion
+    }
+}
