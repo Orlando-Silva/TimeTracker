646a3304e373f0688771d52833474302e0f96589
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-06-26 10:58:37 -0300

    Começo da tela de visualização do perfil do usuário

--- TimeTracker/WPFView/Privado/TelaInicial.xaml.cs
+++ TimeTracker/WPFView/Privado/TelaInicial.xaml.cs
@@ -1,22 +1,30 @@
-﻿using Modelos.Entidades;
+﻿
+#region --Using--
+using System.Windows;
+using Modelos.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-#region --Using--
-using System.Windows;
-
 #endregion
 
 namespace WPFView.Privado
 {
     public partial class TelaInicial : Window
     {
-        #region --Atributos--
+        #region --Construtor--
         public TelaInicial()
         {
             InitializeComponent();
             var usuario = Application.Current.Properties["_user"] as Usuario;
             labelUsuario.Content = $"Olá { usuario.Nome }!";
+        }
+        #endregion
+
+        #region --Eventos--
+        private void PackIcon_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            new UsuarioPerfil().Show();
+            this.Close();
         }
         #endregion
     }
---
+++ TimeTracker/WPFView/Privado/UsuarioPerfil.xaml.cs
@@ -0,0 +1,33 @@
+﻿#region --Using--
+using Modelos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+#endregion
+
+namespace WPFView.Privado
+{
+    /// <summary>
+    /// Interaction logic for UsuarioPerfil.xaml
+    /// </summary>
+    public partial class UsuarioPerfil : Window
+    {
+        public UsuarioPerfil()
+        {
+            InitializeComponent();
+            var usuario = Application.Current.Properties["_user"] as Usuario;
+            LabelLogin.Content = usuario.Login;
+            LabelNome.Content = usuario.Nome;
+        }
+    }
+}
